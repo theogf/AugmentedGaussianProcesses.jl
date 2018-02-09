17571b8072bea79f4b165a9cc196dde95b5760cf
Theo <theo.galyfajou@gmail.com>
2018-02-09 12:34:59 +0100

    Change of the corrections

--- src/correction.jl
+++ src/correction.jl
@@ -3,6 +3,8 @@ module LinResp
 using DAM
 using DataAccess
 using Distributions
+using KernelFunctions
+using Plots
 export GetLastState
 export correctedpredict
 export meancorrection
@@ -36,6 +38,10 @@ function GetLastState(dataset;cluster=false)
 end
 
 
+function logit(x)
+    return 1./(1+exp.(-x))
+end
+
 function correctedpredict(model,X_test)
     n = size(X_test,1)
     ksize = model.nSamples
@@ -49,18 +55,28 @@ function correctedpredict(model,X_test)
     A = kstar*model.invK
     kstarstar = CreateDiagonalKernelMatrix(X_test,model.Kernel_function)
     meanfstar = kstar*model.TopMatrixForPrediction
-    meanfstar_corr = meanfstar + meancorrection(meanfstar,kstar,model.invK,model.μ,model.Σ,model.α)
+    meanfstar_corr = meanfstar + meancorrection(meanfstar,kstar,model.invK,model.μ,model.ζ,model.α)
     covfstar = kstarstar + diag(A*(model.ζ*model.invK-eye(model.nSamples))*transpose(kstar))
-    covfstar_corr = covfstar + varcorrection(meanfstar,kstar,model.invK,model.μ,model.Σ,model.α)
-    predic = zeros(nPoints)
-    for i in 1:nPoints
-        d= Normal(m[i],cov[i])
+    covfstar_corr = covfstar + varcorrection(meanfstar,covfstar,kstar,model.invK,model.μ,model.ζ,model.α;fstar_corr=meanfstar_corr)
+    predic = zeros(n)
+    for i in 1:n
+        d= Normal(meanfstar_corr[i],covfstar_corr[i])
         f=function(x)
             return logit(x)*pdf(d,x)
         end
-        predic[i] = quadgk(f,m[i]-10*cov[i],m[i]+10*cov[i])[1]
+        predic[i] = quadgk(f,meanfstar_corr[i]-10*covfstar_corr[i],meanfstar_corr[i]+10*covfstar_corr[i])[1]
     end
     return predic,meanfstar_corr,covfstar_corr
+end
+
+function correctedlatent(model)
+    var_c = (tanh.(model.α.*0.5).^2-1.0)./(4.*model.α.^2)+(tanh.(model.α.*0.5))./(2.*model.α.^3)
+    mean_corr = model.μ .+ model.ζ*(diag(model.ζ).*model.μ.*var_c)
+    var_corr = diag(model.ζ)
+    correction = model.μ.^2 - mean_corr.^2
+    correction += 0.5*(model.ζ).^2*((model.μ.^2-diag(model.ζ)).*var_c)
+    var_corr += correction
+    return mean_corr,var_corr
 end
 
 function meancorrection(fstar,kstar,invK,μ,Σ,c)
@@ -69,7 +85,19 @@ function meancorrection(fstar,kstar,invK,μ,Σ,c)
     return correction
 end
 
-function varcorrection(fstar,kstar,invK,μ,Σ,c)
-    return 0
+function varcorrection(fstar,covfstar,kstar,invK,μ,Σ,c;fstar_corr=0)
+    var_c = (tanh.(c.*0.5).^2-1.0)./(4.*c.^2)+(tanh.(c.*0.5))./(2.*c.^3)
+    if fstar_corr ==0
+        fstar_corr = fstar + meancorrection(fstar,kstar,invK,μ,Σ,c)
+    end
+    corr_mean = fstar_corr - fstar
+    correction = fstar.^2 - fstar_corr.^2
+    correction += 0.5*((kstar*invK*Σ).^2*((μ.^2+diag(Σ)).*var_c))
+    # println(0.5*((kstar*invK*Σ).^2*((μ.^2-diag(Σ)).*var_c)))
+    display(Plots.plot(covfstar,correction,t=:scatter))
+
+    println(correction)
+    return correction
 end
+
 end
--- test/matrix_convergence_sampler.jl
+++ test/matrix_convergence_sampler.jl
@@ -26,7 +26,7 @@ y_test = y_data[fold_separation[i]:(fold_separation[i+1])-1]
 X = X_data[vcat(collect(1:fold_separation[i]-1),collect(fold_separation[i+1]:nSamples)),:]
 y = y_data[vcat(collect(1:fold_separation[i]-1),collect(fold_separation[i+1]:nSamples))]
 
-var=100.0; θ=5.0; ϵ=1e-10; γ=0.001
+var=1.0; θ=5.0; ϵ=1e-10; γ=0.001
 nBurnin = 100; nSamples = 10000+nBurnin;
 MaxIter = 5000 #Maximum number of iterations for every algorithm
 
@@ -52,47 +52,97 @@ println("Accuracy : Gibbs $(1-sum(1-y_test.*y_gibbs)/(2*length(y_test)))
                     VGPC  $(1-sum(1-y_test.*y_vgpc)/(2*length(y_test)))
                     XGPC  $(1-sum(1-y_test.*y_vi)/(2*length(y_test)))")
 
-
-
-
-function save_values(dataset,gibbs_m,vgpc_m,vi_m,X_test,y_test)
-    p_gibbs = gibbs_m.predictproba(X_test)
-    f_gibbs,covf_gibbs = DAM.computefstar(gibbs_m,X_test)
-    p_vgpc, = vgpc_m[:predict_y](X_test)
-    f_vgpc, covf_vgpc = vgpc_m[:predict_f](X_test)
-    p_vi = vi_m.predictproba(X_test)
-    f_vi, covf_vi = DAM.computefstar(vi_m,X_test)
-    p_vi_corr, f_vi_corr, covf_vi_corr = LinResp.correctedpredict(vi_m,X_test)
+function save_values(dataset,X_test,y_test,gibbs_m=0,vgpc_m=0,vi_m=0)
     top_fold = "/home/theo/XGPC/results/ComparisonExp"*dataset
     if !isdir(top_fold); mkdir(top_fold); end;
-    data_gibbs = hcat(p_gibbs,f_gibbs,covf_gibbs)
-    writedlm(top_fold*"/Gibbs",data_gibbs)
-    data_vgpc = hcat(p_vgpc,f_vgpc,covf_vgpc)
-    writedlm(top_fold*"/VGPC",data_vgpc)
-    data_vi = hcat(p_vi,f_vi,covf_vi)
-    writedlm(top_fold*"/XGPC",data_vi)
+    if vi_m != 0
+        p_vi = vi_m.predictproba(X_test)
+        f_vi, covf_vi = DAM.computefstar(vi_m,X_test)
+        p_vi_corr, f_vi_corr, covf_vi_corr = LinResp.correctedpredict(vi_m,X_test)
+        data_vi = hcat(p_vi,f_vi,covf_vi)
+        data_vi_latent = hcat(vi_m.μ,diag(vi_m.ζ))
+        writedlm(top_fold*"/XGPC",data_vi)
+        writedlm(top_fold*"/XGPC_latent",data_vi_latent)
+        data_vi_corr = hcat(p_vi_corr,f_vi_corr,covf_vi_corr)
+        f_latent, covf_latent = LinResp.correctedlatent(vi_m)
+        data_vi_latent_corr = hcat(f_latent,covf_latent)
+        writedlm(top_fold*"/CorrXGPC",data_vi_corr)
+        writedlm(top_fold*"/CorrXGPC_latent",data_vi_latent_corr)
+    end
+    if gibbs_m != 0
+        p_gibbs = gibbs_m.predictproba(X_test)
+        f_gibbs,covf_gibbs = DAM.computefstar(gibbs_m,X_test)
+        data_gibbs = hcat(p_gibbs,f_gibbs,covf_gibbs)
+        data_gibbs_latent =  hcat(gibbs_m.μ,diag(gibbs_m.ζ))
+        writedlm(top_fold*"/Gibbs",data_gibbs)
+        writedlm(top_fold*"/Gibbs_latent",data_gibbs_latent)
+    end
+    if vgpc_m != 0
+        p_vgpc, = vgpc_m[:predict_y](X_test)
+        f_vgpc, covf_vgpc = vgpc_m[:predict_f](X_test)
+        data_vgpc = hcat(p_vgpc,f_vgpc,covf_vgpc)
+        writedlm(top_fold*"/VGPC",data_vgpc)
+    end
+
 end
 
-save_values(DatasetName,gibbsmodel,vgpcmodel,vimodel,X_test,y_test)
-
-
-
-function truth_comparison(dataset)
+function truth_predict_comparison(dataset)
     truth = readdlm("/home/theo/XGPC/results/ComparisonExp"*dataset*"/Gibbs")
     p_truth = truth[:,1]; f_truth = truth[:,2]; covf_truth = truth[:,3]
     xgpc = readdlm("/home/theo/XGPC/results/ComparisonExp"*dataset*"/XGPC")
     p_xgpc = xgpc[:,1]; f_xgpc = xgpc[:,2]; covf_xgpc = xgpc[:,3]
+    xgpc_corr = readdlm("/home/theo/XGPC/results/ComparisonExp"*dataset*"/CorrXGPC")
+    p_xgpc_corr = xgpc_corr[:,1]; f_xgpc_corr = xgpc_corr[:,2]; covf_xgpc_corr = xgpc_corr[:,3]
     vgpc = readdlm("/home/theo/XGPC/results/ComparisonExp"*dataset*"/VGPC")
     p_vgpc = vgpc[:,1]; f_vgpc = vgpc[:,2]; covf_vgpc = vgpc[:,3]
     p1 = plot(p_truth,p_xgpc,t=:scatter, lab="p XGPC", xlim=(0,1),ylim=(0,1))
+    p1 = plot!(p1,x->x,lab="")
     p2 = plot(f_truth,f_xgpc,t=:scatter, lab="μ XGPC",title="θ=$(θ), var=$(var)")
-    p3 = plot(covf_truth,covf_xgpc,t=:scatter, lab="σ XGPC")
-    p4 = plot(p_truth,p_vgpc,t=:scatter,lab="p VGPC", xlim=(0,1),ylim=(0,1))
-    p5 = plot(p_truth,f_vgpc,t=:scatter,lab="μ VGPC")
-    p6 = plot(p_truth,covf_vgpc,t=:scatter,lab="σ VGPC")
-    plot(p1,p2,p3,p4,p5,p6,layout=(2,3))
+    p2 = plot!(p2,x->x,lab="")
+    p3 = plot(covf_truth,covf_xgpc,t=:scatter, lab="σ XGPC", zcolor=f_xgpc)
+    p3 = plot!(p3,x->x,lab="")
+    p4 = plot(p_truth,p_xgpc_corr,t=:scatter, lab="p corr. XGPC", xlim=(0,1),ylim=(0,1))
+    p4 = plot!(p4,x->x,lab="")
+    p5 = plot(f_truth,f_xgpc_corr,t=:scatter, lab="μ corr. XGPC")
+    p5 = plot!(p5,x->x,lab="")
+    p6 = plot(covf_truth,covf_xgpc_corr,t=:scatter, zcolor=f_xgpc_corr,lab="σ corr. XGPC")
+    p6 = plot!(p6,x->x,lab="")
+    p7 = plot(p_truth,abs.(p_truth-p_xgpc)-abs.(p_truth-p_xgpc_corr),t=:scatter,zcolor=f_truth,lab="p improvement", xlim=(0,1))
+    p7 = plot!(p7,x->0,lab="")
+    p8 = plot(f_truth,abs.(f_xgpc - f_truth)-abs.(f_xgpc_corr-f_truth),t=:scatter,lab="μ improvement")
+    p8 = plot!(p8,x->0,lab="")
+    p9 = plot(covf_truth,abs.(covf_xgpc-covf_truth)-abs.(covf_xgpc_corr-covf_truth),t=:scatter,lab="σ improvement")
+    p9 = plot!(p9,x->0,lab="")
+    plot(p1,p2,p3,p4,p5,p6,p7,p8,p9,layout=(3,3))
 end
 
-p = truth_comparison("German")
+function truth_latent_comparison(dataset)
+    truth = readdlm("/home/theo/XGPC/results/ComparisonExp"*dataset*"/Gibbs_latent")
+    f_truth = truth[:,1]; covf_truth = truth[:,2]
+    xgpc = readdlm("/home/theo/XGPC/results/ComparisonExp"*dataset*"/XGPC_latent")
+    f_xgpc = xgpc[:,1]; covf_xgpc = xgpc[:,2]
+    xgpc_corr = readdlm("/home/theo/XGPC/results/ComparisonExp"*dataset*"/CorrXGPC_latent")
+    f_xgpc_corr = xgpc_corr[:,1]; covf_xgpc_corr = xgpc_corr[:,2]
+    p2 = plot(f_truth,f_xgpc,t=:scatter, lab="μ XGPC",title="$dataset : θ=$(θ), var=$(var)")
+    p2 = plot!(p2,x->x,lab="")
+    p3 = plot(covf_truth,covf_xgpc,t=:scatter, lab="σ XGPC", zcolor=f_xgpc)
+    p3 = plot!(p3,x->x,lab="")
+    p5 = plot(f_truth,f_xgpc_corr,t=:scatter, lab="μ corr. XGPC")
+    p5 = plot!(p5,x->x,lab="")
+    p6 = plot(covf_truth,covf_xgpc_corr,t=:scatter, zcolor=f_xgpc_corr,lab="σ corr. XGPC")
+    p6 = plot!(p6,x->x,lab="")
+    p8 = plot(f_truth,abs.(f_xgpc - f_truth)-abs.(f_xgpc_corr-f_truth),t=:scatter,lab="μ improvement")
+    p8 = plot!(p8,x->0,lab="")
+    p9 = plot(covf_truth,abs.(covf_xgpc-covf_truth)-abs.(covf_xgpc_corr-covf_truth),t=:scatter,lab="σ improvement")
+    p9 = plot!(p9,x->0,lab="")
+    plot(p2,p3,p5,p6,p8,p9,layout=(3,2))
+end
+DatasetName = "German"
+DatasetName = "Diabetis"
+save_values(DatasetName,X_test,y_test,gibbsmodel,vgpcmodel,vimodel)
+
+p = truth_predict_comparison(DatasetName)
 display(p)
+p2 = truth_latent_comparison(DatasetName)
+display(p2)
 savefig(p,"/home/theo/XGPC/plots/ModelApproximation/"*DatasetName*"/theta_$(θ)_var_$(var).png")
