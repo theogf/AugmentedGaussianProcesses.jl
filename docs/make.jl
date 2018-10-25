6245b83573decea0c2397cca07953d7ab6a8858c
Théo Galy-Fajou <theo.galyfajou@gmail.com>
2018-10-25 18:39:47 +0200

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -3,6 +3,7 @@ using Documenter, OMGP
 makedocs(modules=[OMGP])
 
 deploydocs(
+    deps = Deps.pip("mkdocs", "python-markdown-math"),
     repo = "github.com/theogf/OMGP.jl.git",
     julia = "1.0"
 )
