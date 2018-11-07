29ac34625eac7ca5f214451abbf1f0f07c749590
autodocs <autodocs>
2018-11-07 18:41:27 +0000

    build based on 8b5d5e4

--- dev/search_index.js
+++ dev/search_index.js
@@ -21,15 +21,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Home",
     "title": "Authors",
     "category": "section",
-    "text": "Theo Galy-Fajou PhD Student at Technical University of Berlin.\nFlorian Wenzel PhD Student at Technical University of Kaiserslautern"
-},
-
-{
-    "location": "#License-1",
-    "page": "Home",
-    "title": "License",
-    "category": "section",
-    "text": "AugmentedGaussianProcesses.jl is licensed under the MIT \"Expat\" license; see LICENSE for the full license text."
+    "text": "Théo Galy-Fajou PhD Student at Technical University of Berlin.\nFlorian Wenzel PhD Student at Technical University of Kaiserslautern and Humboldt University of Berlin"
 },
 
 {
@@ -57,6 +49,14 @@ var documenterSearchIndex = {"docs": [
 },
 
 {
+    "location": "#License-1",
+    "page": "Home",
+    "title": "License",
+    "category": "section",
+    "text": "AugmentedGaussianProcesses.jl is licensed under the MIT \"Expat\" license; see LICENSE for the full license text."
+},
+
+{
     "location": "background/#",
     "page": "Background",
     "title": "Background",
@@ -65,17 +65,9 @@ var documenterSearchIndex = {"docs": [
 },
 
 {
-    "location": "background/#The-bits-of-science-behing-it-1",
+    "location": "background/#The-bits-of-math-and-science-behind-it-1",
     "page": "Background",
-    "title": "The bits of science behing it",
-    "category": "section",
-    "text": ""
-},
-
-{
-    "location": "background/#!!!-In-construction-!!!-1",
-    "page": "Background",
-    "title": "!!! In construction !!!",
+    "title": "The bits of math and science behind it",
     "category": "section",
     "text": ""
 },
@@ -85,7 +77,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Background",
     "title": "Gaussian Processes",
     "category": "section",
-    "text": ""
+    "text": "To quote Wikipedia \"A Gaussian process is a stochastic process (a collection of random variables indexed by time or space), such that every finite collection of those random variables has a multivariate normal distribution, i.e. every finite linear combination of them is normally distributed. The distribution of a Gaussian process is the joint distribution of all those (infinitely many) random variables, and as such, it is a distribution over functions with a continuous domain, e.g. time or space.\"For a detailed understanding of Gaussian processes, check the wonderful book of Rasmussen and Williams and for a quick introduction, check this tutorial by Zoubin GhahramaniGaussian Processes are extremely practical model since they are non-parametric and are Bayesian. However the basic model is limited to regression with Gaussian noise and does not scale very well to large datasets (>1000 samples). The Augmented Gaussian Processes solve both these problems by adding inducing points as well as transforming the likelihood to get efficient variational inference."
 },
 
 {
@@ -93,7 +85,23 @@ var documenterSearchIndex = {"docs": [
     "page": "Background",
     "title": "Augmented Gaussian Processes",
     "category": "section",
-    "text": ""
+    "text": "We are interested in models which consist of a GP prior on a latent function fsim textGP(0k), where k is the kernel function and the data y is connected to f via a non-conjugate likelihood p(yf) . We now aim on finding an augmented representation of the model which renders the model conditionally conjugate. Let omega be potential augmentation, then the augmented joint distribution isp(yfomega) =p(yfomega)p(omega)p(f).The original model can be restored by marginalizing omega, i.e. p(yf) =int p(yfomega)domega.The  goal  is  to  find  an  augmentation omega,  such  that  the  augmented  likelihood p(yfomega) becomes conjugate to the prior distributions p(f) and p(omega) and the expectations of the log complete conditional distributions log p(fomegay) and log p(omegafy) can be computed in closed-form."
+},
+
+{
+    "location": "background/#How-to-find-a-suitable-augmentation?-1",
+    "page": "Background",
+    "title": "How to find a suitable augmentation?",
+    "category": "section",
+    "text": "Many popular likelihood functions can be expressed as ascale mixture of Gaussiansp(yf) =int N(yBftextdiag(omega^1))p(omega)domegawhere B is a matrix (Palmer et al., 2006).  This representation directly leads to the augmented likelihood p(yomegaf) =N(yBftextdiag(omega^1)) which is conjugate in f, i.e. the posterior is Gaussian again."
+},
+
+{
+    "location": "background/#Inference-in-the-augmented-model-1",
+    "page": "Background",
+    "title": "Inference in the augmented model",
+    "category": "section",
+    "text": "We n assume that the augmentation, discussed in the previous section, was successful and we obtained an augmented model p(yfomega) = p(yfomega)p(f)p(omega) that is conditionally conjugate. In a conditionally conjugate model variational inference is easy and block coordinate ascent updates can be computed in closed-form. We follow as structured mean-field approach and assume a decoupling between the latent GP f and the auxiliary variable omega in the variational distribution q(fomega) = q(f) q(omega).  We alternate between updating q(f) and q(omega) by using the typical coordinate ascent (CAVI) updates building on expectations of the log complete conditionals.The hyperparameter of the latent GP (e.g. length scale) are learned by optimizing the variational lower bound as function of the hyper parameters. We alternate between updating the variational parameters and the hyperparameters."
 },
 
 {
@@ -101,7 +109,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Background",
     "title": "Sparse Gaussian Processes",
     "category": "section",
-    "text": ""
+    "text": "Direct inference for GPs has a cubic computational complexity mathcalO(N^3). To scale our model to big datasets we approximate the latent GP by a sparse GP building on inducing points. This reduces the complexity to mathcalO(M^3), where M is the number of inducing points. Using inducing points allows us to employ stochastic variational inference (SVI) that computes the updates based on mini-batches of the data."
 },
 
 {
@@ -121,7 +129,7 @@ var documenterSearchIndex = {"docs": [
 },
 
 {
-    "location": "userguide/#Initialization-1",
+    "location": "userguide/#init-1",
     "page": "User Guide",
     "title": "Initialization",
     "category": "section",
@@ -133,7 +141,7 @@ var documenterSearchIndex = {"docs": [
     "page": "User Guide",
     "title": "Sparse vs FullBatch",
     "category": "section",
-    "text": "A FullBatchModel is a normal GP model, where the variational distribution is optimized over all the training points and no stochastic updates are possible. It is therefore fitted for small datasets (~10^3 samples)\nA SparseModel is a GP model augmented with inducing points. The optimization is done on those points, allowing for stochastic updates and huge scalability. The counterpart can be a slightly lower accuracy and the need to select the number and the location of the inducing points (however this is a problem currently worked on).To pick between the two add Batch or Sparse in front of the name of the desired model."
+    "text": "A FullBatchModel is a normal GP model, where the variational distribution is optimized over all the training points and no stochastic updates are possible. It is therefore fitted for small datasets (~10^3 samples)\nA SparseModel is a GP model augmented with inducing points. The optimization is done on those points, allowing for stochastic updates and huge scalability. The counterpart can be a slightly lower accuracy and the need to select the number and the location of the inducing points (however this is a problem currently worked on).To pick between the two add Batch or Sparse in front of the name of the desired model.A better common interface is currently being developed."
 },
 
 {
@@ -141,7 +149,7 @@ var documenterSearchIndex = {"docs": [
     "page": "User Guide",
     "title": "Regression",
     "category": "section",
-    "text": "For regression one can use the GPRegression or StudentT model. The latter is using the Student-T likelihood and is therefore a lot more robust to ouliers."
+    "text": "For regression one can use the GPRegression or StudentT model. The first one is the vanilla GP with Gaussian noise while the second is using the Student-T likelihood and is therefore a lot more robust to ouliers."
 },
 
 {
@@ -149,7 +157,7 @@ var documenterSearchIndex = {"docs": [
     "page": "User Guide",
     "title": "Classification",
     "category": "section",
-    "text": "For classification one can use the XGPC using the logistic link or the BSVM model based on the frequentist SVM."
+    "text": "For classification one can select the XGPC model using the logistic link or the BSVM model based on the frequentist SVM."
 },
 
 {
@@ -169,7 +177,7 @@ var documenterSearchIndex = {"docs": [
 },
 
 {
-    "location": "userguide/#Training-1",
+    "location": "userguide/#train-1",
     "page": "User Guide",
     "title": "Training",
     "category": "section",
@@ -177,7 +185,7 @@ var documenterSearchIndex = {"docs": [
 },
 
 {
-    "location": "userguide/#Prediction-1",
+    "location": "userguide/#pred-1",
     "page": "User Guide",
     "title": "Prediction",
     "category": "section",
@@ -221,7 +229,23 @@ var documenterSearchIndex = {"docs": [
     "page": "Kernels",
     "title": "Kernels (Covariance functions)",
     "category": "section",
-    "text": ""
+    "text": "The kernel function or covariance function is a crucial part of Gaussian Processes. It determines the covariance matrices between set of points, and its behaviour and parameters determines almost completely a GP behaviour."
+},
+
+{
+    "location": "kernel/#Kernels-available-1",
+    "page": "Kernels",
+    "title": "Kernels available",
+    "category": "section",
+    "text": "To get a short introduction of some covariance functions available one can look at these Slides from RasmussenWe define theta_i as the lengthscale (in case of IsoKernel theta_i=thetaforall i) and sigma is the variance In this package covariance functions are progressively added for now the available kernels are :RBF Kernel or Squared Exponential Kernelk(xx) = sigma expleft(-frac12sum_i=1^Dfrac(x_i-x_i)^2theta_i^2right)Matern 3/2 Kernelk(xx)= sigmaleft(1+sqrt3sum frac(x_i - x_i)^2theta_i^2right)expleft(-sqrt3sum frac(x_i - x_i)^2theta_i^2right)More are coming, check the github projects for updates ."
+},
+
+{
+    "location": "kernel/#Hyperparameter-optimization-1",
+    "page": "Kernels",
+    "title": "Hyperparameter optimization",
+    "category": "section",
+    "text": "The advantage of Gaussian Processes is that it is possible to optimize all the hyperparameters of the model by optimizing the lower bound on the loglikelihood. One can compute the gradient of it and apply a classical gradient descent algorithm.Unlike most other packages, the derivatives are all computed analytically. Since the hyperparameters intervene in gradients one needs to compute the matrix derivatives via the kernel derivatives. If K was defined via k(xx) then :$ \\frac{d K}{d\\theta}  = J_\\theta$Where J_theta was defined via fracdk(xx)dtheta, the rest of the work is simply matrix algebra."
 },
 
 {
@@ -353,19 +377,19 @@ var documenterSearchIndex = {"docs": [
 },
 
 {
-    "location": "api/#AugmentedGaussianProcesses.train!",
+    "location": "api/#AugmentedGaussianProcesses.train!-Tuple{OfflineGPModel}",
     "page": "API",
     "title": "AugmentedGaussianProcesses.train!",
-    "category": "function",
-    "text": "Function to train the given Online GP model, there are options to change the number of max iterations, give a callback function that will take the model and the actual step as arguments and give a convergence method to stop the algorithm given specific criteria\n\n\n\n\n\nFunction to train the given GP model, there are options to change the number of max iterations, give a callback function that will take the model and the actual step as arguments and give a convergence method to stop the algorithm given specific criteria\n\n\n\n\n\n"
+    "category": "method",
+    "text": "Function to train the given GP model, there are options to change the number of max iterations, give a callback function that will take the model and the actual step as arguments and give a convergence method to stop the algorithm given specific criteria\n\n\n\n\n\n"
 },
 
 {
-    "location": "api/#AugmentedGaussianProcesses.regpredict",
+    "location": "api/#AugmentedGaussianProcesses.regpredict-Tuple{GPModel,AbstractArray}",
     "page": "API",
     "title": "AugmentedGaussianProcesses.regpredict",
-    "category": "function",
-    "text": "Return the mean of the predictive distribution of f\n\n\n\n\n\nReturn the mean of the predictive distribution of f\n\n\n\n\n\n"
+    "category": "method",
+    "text": "Return the mean of the predictive distribution of f\n\n\n\n\n\n"
 },
 
 {
@@ -429,15 +453,63 @@ var documenterSearchIndex = {"docs": [
     "page": "API",
     "title": "Functions and methods",
     "category": "section",
-    "text": "train!\nregpredict\nregpredictproba\nstudenttpredict\nstudenttpredictproba\nlogitpredict\nlogitpredictproba\nsvmpredict\nsvmpredictproba"
+    "text": "train!(::OfflineGPModel)\nregpredict(::GPModel,::AbstractArray)\nregpredictproba\nstudenttpredict\nstudenttpredictproba\nlogitpredict\nlogitpredictproba\nsvmpredict\nsvmpredictproba"
 },
 
 {
-    "location": "api/#Internals-1",
+    "location": "api/#AugmentedGaussianProcesses.KernelModule.RBFKernel",
     "page": "API",
-    "title": "Internals",
+    "title": "AugmentedGaussianProcesses.KernelModule.RBFKernel",
+    "category": "type",
+    "text": "Radial Basis Function Kernel also called RBF or SE(Squared Exponential)\n\n\n\n\n\n"
+},
+
+{
+    "location": "api/#Kernels-1",
+    "page": "API",
+    "title": "Kernels",
     "category": "section",
-    "text": ""
+    "text": "RBFKernel"
+},
+
+{
+    "location": "api/#AugmentedGaussianProcesses.KernelModule.kernelmatrix",
+    "page": "API",
+    "title": "AugmentedGaussianProcesses.KernelModule.kernelmatrix",
+    "category": "function",
+    "text": "Create the covariance matrix between the matrix X1 and X2 with the covariance function kernel\n\n\n\n\n\nCompute the covariance matrix of the matrix X, optionally only compute the diagonal terms\n\n\n\n\n\n"
+},
+
+{
+    "location": "api/#AugmentedGaussianProcesses.KernelModule.kernelmatrix!",
+    "page": "API",
+    "title": "AugmentedGaussianProcesses.KernelModule.kernelmatrix!",
+    "category": "function",
+    "text": "Compute the covariance matrix between the matrix X1 and X2 with the covariance function kernel in preallocated matrix K\n\n\n\n\n\nCompute the covariance matrix of the matrix X in preallocated matrix K, optionally only compute the diagonal terms\n\n\n\n\n\n"
+},
+
+{
+    "location": "api/#AugmentedGaussianProcesses.KernelModule.getvariance",
+    "page": "API",
+    "title": "AugmentedGaussianProcesses.KernelModule.getvariance",
+    "category": "function",
+    "text": "Return the variance of the kernel\n\n\n\n\n\n"
+},
+
+{
+    "location": "api/#AugmentedGaussianProcesses.KernelModule.getlengthscales",
+    "page": "API",
+    "title": "AugmentedGaussianProcesses.KernelModule.getlengthscales",
+    "category": "function",
+    "text": "Return the lengthscale of the IsoKernel\n\n\n\n\n\nReturn the lengthscales of the ARD Kernel\n\n\n\n\n\n"
+},
+
+{
+    "location": "api/#Kernel-functions-1",
+    "page": "API",
+    "title": "Kernel functions",
+    "category": "section",
+    "text": "kernelmatrix\nkernelmatrix!\ngetvariance\ngetlengthscales"
 },
 
 {
