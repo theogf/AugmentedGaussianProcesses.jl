86aceee4e806808c3a7eab6b905e68cae843d299
Théo Galy-Fajou <theo.galyfajou@gmail.com>
2019-01-27 17:33:59 +0100

    First basis for refunding of classes

---
+++ src/models/SVGP.jl
@@ -0,0 +1,19 @@
+""" Class for sparse variational Gaussian Processes """
+
+mutable struct SVGP{L<:Likelihood,I<:Inference,T<:Real,A<:AbstractArray} <: GP{L,I,T,A}
+    X::AbstractMatrix{T} #Feature vectors
+    y::Vector #Output (-1,1 for classification, real for regression, matrix for multiclass)
+    nSample::Int64 # Number of data points
+    nDim::Int64 # Number of covariates per data point
+    nFeature::Int64 # Number of features of the GP (equal to number of points)
+    nLatent::Int64 # Number pf latent GPs
+    μ::AbstractVector{AbstractVector}
+    Σ::AbstractVector{AbstractMatrix}
+    Kmm::AbstractVector{AbstractMatrix}
+    invKmm::AbstractVector{AbstractMatrix}
+    Knm::AbstractVector{AbstractMatrix}
+    κ::AbstractVector{AbstractMatrix}
+    K̃::AbstractVector{AbstractVector}
+    likelihood::Likelihood
+    inference::Inference
+end
---
+++ src/models/VGP.jl
@@ -0,0 +1,17 @@
+""" Class for variational Gaussian Processes models"""
+
+mutable struct VGP{L<:Likelihood,I<:Inference,T<:Real,A<:AbstractArray} <: GP{L,I,T,A}
+    X::AbstractMatrix{T} #Feature vectors
+    y::Vector #Output (-1,1 for classification, real for regression, matrix for multiclass)
+    nSample::Int64 # Number of data points
+    nDim::Int64 # Number of covariates per data point
+    nFeature::Int64 # Number of features of the GP (equal to number of points)
+    nLatent::Int64 # Number pf latent GPs
+    μ::AbstractVector{AbstractVector}
+    Σ::AbstractVector{AbstractMatrix}
+    Knn::AbstractVector{AbstractMatrix}
+    invKnn::AbstractVector{AbstractMatrix}
+    likelihood::Likelihood
+    inference::Inference
+    verbose::Int64 #Level of printing information
+end
