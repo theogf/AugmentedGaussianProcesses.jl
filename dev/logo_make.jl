076111f3ebae5b1aaea2e024282bc2a9e652f609
Theo Galy-Fajou <theo.galyfajou@gmail.com>
2019-05-05 20:07:09 +0200

    3 logos versions

--- dev/logo_make.jl
+++ dev/logo_make.jl
@@ -23,7 +23,7 @@ gpgreen = GP(Xgreen,ygreen,k,noise=noise)
 gpred = GP(Xred,yred,k,noise=noise)
 gppurple = GP(Xpurple,yred,k,noise=noise)
 
-grid = collect(range(-1,9.5,length=1000))
+grid = collect(range(-1,9.5,length=500))
 
 pgreen,siggreen = proba_y(gpgreen,grid);siggreen = sqrt.(siggreen)
 pred,sigred = proba_y(gpred,grid); sigred = sqrt.(sigred)
@@ -39,29 +39,32 @@ function f(mu,sigma)
     end
 end
 
-ygrid = range(-2,3,length=200)
+ygrid = range(-0.3,0.3,length=50)
 
 
-ksig = 2.0
+ksig = 0.2
 # fgrid = f.(pgreen,siggreen)
-Ygreen = hcat([f(mu,sigma).(ygrid) for (mu,sigma) in zip(pgreen,siggreen)]...)
-Yred = hcat([f(mu,sigma).(ygrid) for (mu,sigma) in zip(pred,sigred)]...)
-Ypurple = hcat([f(mu,sigma).(ygrid) for (mu,sigma) in zip(ppurple,sigpurple)]...)
+Ygreen = hcat([f(mu,ksig*sigma).(ygrid) for (mu,sigma) in zip(pgreen,siggreen)]...)
+Yred = hcat([f(mu,ksig*sigma).(ygrid) for (mu,sigma) in zip(pred,sigred)]...)
+Ypurple = hcat([f(mu,ksig*sigma).(ygrid) for (mu,sigma) in zip(ppurple,sigpurple)]...)
 Xgrid = hcat([j for i in grid, j in ygrid][:],[i for i in grid, j in ygrid][:])
 
-scene = plot(grid,pgreen,color=green,linewidth=lw,backgroundcolor=:white)
-fill_between!(grid,pgreen-nsig*siggreen,pgreen+nsig*siggreen,where = trues(size(grid)),color=RGBA(green,alpha),transparency=true)
+scene = Scene(backgroundcolor=:white)
+image!(grid,ygrid,RGBA.(green,alpha*Ygreen'))
+image!(grid,ygrid,RGBA.(red,alpha*Yred'))
+image!(grid,ygrid,RGBA.(purple,alpha*Ypurple'))
+
+plot!(grid,pgreen,color=green,linewidth=lw)
+# fill_between!(grid,pgreen-nsig*siggreen,pgreen+nsig*siggreen,where = trues(size(grid)),color=RGBA(green,alpha),transparency=true)
 plot!(grid,pred,color=red,linewidth=lw)
-fill_between!(grid,pred-nsig*sigred,pred+nsig*sigred,where = trues(size(grid)),color=RGBA(red,alpha))
+# fill_between!(grid,pred-nsig*sigred,pred+nsig*sigred,where = trues(size(grid)),color=RGBA(red,alpha))
 plot!(grid,ppurple,color=purple,linewidth=lw)
-fill_between!(grid,ppurple-nsig*sigpurple,ppurple+nsig*sigpurple,where = trues(size(grid)),color=RGBA(purple,alpha)  )
+# fill_between!(grid,ppurple-nsig*sigpurple,ppurple+nsig*sigpurple,where = trues(size(grid)),color=RGBA(purple,alpha)  )
 scatter!([p_green,p_red,p_purple],color=[green,red,purple],markersize=ms)#,limits=FRect2D((-1.0,-1.0),(100.0,2.5)))
 text!("AugmentedGaussianProcesses.jl",position=(2.1,0.0),font="Franklin Gothic Medium",textsize=0.5,align=(:left,:center),color=:white)
+# image(eachrow(Xgrid)...,color=RGBA.(green,Ygreen[:]))
 scene[Axis][:showgrid] = (false,false)
 scene[Axis][:showaxis] = (false,false)
 scene[Axis][:ticks,:textsize] = (0.0,0.0)
 scene[Axis][:names][:axisnames] = ("","")
-save("logo.png",scene)
-# scatter(eachrow(Xgrid)...,color=RGBA.(green,Ygreen[:]))
-# scatter(eachrow(Xgrid)...,color=RGBA.(red,Yred[:]))
-# scatter(eachrow(Xgrid)...,color=RGBA.(purple,Ypurple[:]))
+save("logo3.png",scene)
