663435795aeb69952ff090980bb6b0630905c842
Théo Galy-Fajou <theo.galyfajou@gmail.com>
2019-08-02 15:23:36 +0200

    Correction ELBO term Student-T

--- src/likelihood/studentt.jl
+++ src/likelihood/studentt.jl
@@ -1,7 +1,7 @@
 """
 **Student-T likelihood**
 
-Student-t likelihood for regression: ``\\frac{\\Gamma((\\nu+1)/2)}{\\sqrt{\\nu\\pi}\\Gamma(\\nu/2)}\\left(1+t^2/\\nu\\right)^{(-(\\nu+1)/2)}``
+Student-t likelihood for regression: ``\\frac{\\Gamma((\\nu+1)/2)}{\\sqrt{\\nu\\pi}\\sigma\\Gamma(\\nu/2)}\\left(1+(y-f)^2/(\\sigma^2\\nu)\\right)^{(-(\\nu+1)/2)}``
 see [wiki page](https://en.wikipedia.org/wiki/Student%27s_t-distribution)
 
 ```julia
@@ -13,7 +13,7 @@ StudentTLikelihood(ν::T,σ::Real=one(T)) #ν is the number of degrees of freedo
 
 For the analytical solution, it is augmented via:
 ```math
-p(y|f,\\omega) = \\mathcal{N}(y|f,\\omega)
+p(y|f,\\omega) = \\mathcal{N}(y|f,\\sigma^2\\omega)
 ```
 Where ``\\omega \\sim \\mathcal{IG}(\\frac{\\nu}{2},\\frac{\\nu}{2})`` where ``\\mathcal{IG}`` is the inverse gamma distribution
 See paper [Robust Gaussian Process Regression with a Student-t Likelihood](http://www.jmlr.org/papers/volume12/jylanki11a/jylanki11a.pdf)
@@ -56,7 +56,7 @@ function Base.show(io::IO,model::StudentTLikelihood{T}) where T
 end
 
 function compute_proba(l::StudentTLikelihood{T},μ::AbstractVector{T},σ²::AbstractVector{T}) where {T<:Real}
-    return μ,max.(σ²,0.0).+0.5*l.ν*l.σ/(0.5*l.ν-1)
+    return μ,max.(σ²,0.0).+0.5*l.ν*l.σ^2/(0.5*l.ν-1)
 end
 
 ## Local Updates ##
@@ -93,15 +93,15 @@ end
 
 function expecLogLikelihood(model::VGP{T,<:StudentTLikelihood,<:AnalyticVI}) where {T}
     tot = -0.5*model.nLatent*model.nSample*(log(twoπ*model.likelihood.σ))
-    tot += -0.5.*sum(broadcast((α,β)->sum(digamma(α).-log.(β)), model.likelihood.α,model.likelihood.β))
-    tot += -0.5.*sum(broadcast((θ,Σ,μ,y)->dot(θ,Σ+abs2.(μ)-2.0*μ.*y-abs2.(y)),model.likelihood.θ,diag.(model.Σ),model.μ,model.y))
+    tot += -sum(broadcast((α,β)->sum(log.(β).-digamma(α)), model.likelihood.α,model.likelihood.β))
+    tot += -0.5.*sum(broadcast((θ,Σ,μ,y)->dot(θ,Σ+abs2.(μ)-2.0*μ.*y+abs2.(y)),model.likelihood.θ,diag.(model.Σ),model.μ,model.y))
     return tot
 end
 
 function expecLogLikelihood(model::SVGP{T,<:StudentTLikelihood,<:AnalyticVI}) where {T}
     tot = -0.5*model.nLatent*model.inference.nSamplesUsed*(log(twoπ*model.likelihood.σ))
-    tot += -0.5.*sum(broadcast(β->sum(digamma(model.likelihood.α).-log.(β)),model.likelihood.β))
-    tot += -0.5.*sum(broadcast((θ,K̃,κ,Σ,κμ,y)->dot(θ,(K̃+opt_diag(κ*Σ,κ)+abs2.(κμ)-2.0*(κμ).*y-abs2.(y))),model.likelihood.θ,model.K̃,model.κ,model.Σ,model.κ.*model.μ,model.inference.y))
+    tot += -sum(broadcast(β->sum(digamma(model.likelihood.α).-log.(β)),model.likelihood.β))
+    tot += -0.5.*sum(broadcast((θ,K̃,κ,Σ,κμ,y)->dot(θ,(K̃+opt_diag(κ*Σ,κ)+abs2.(κμ)-2.0*(κμ).*y+abs2.(y))),model.likelihood.θ,model.K̃,model.κ,model.Σ,model.κ.*model.μ,model.inference.y))
     return model.inference.ρ*tot
 end
 
