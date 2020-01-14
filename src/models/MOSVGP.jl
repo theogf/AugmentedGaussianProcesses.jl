e6e449f1ab14853d7d686e715724a42d1560e23c
Theo Galy-Fajou <theo.galyfajou@gmail.com>
2020-01-14 16:54:30 +0100

    Final final

--- src/models/MOSVGP.jl
+++ src/models/MOSVGP.jl
@@ -197,24 +197,26 @@ end
 
 
 @traitfn function update_A!(model::TGP) where {T,TGP<:AbstractGP{T};IsMultiOutput{TGP}}
-    μ_f = mean_f.(model.f) # κμ
-    Σ_f = diag_cov_f.(model.f) #Diag(K̃ + κΣκ)
-    ∇Eμ = ∇E_μ.(model.likelihood,model.inference.vi_opt[1:1],get_y(model))
-    ∇EΣ = ∇E_Σ.(model.likelihood,model.inference.vi_opt[1:1],get_y(model))
-    new_A = zero(model.A)
-    ∇A = zero(model.A)
-    for t in 1:model.nTask
-        for j in 1:model.nf_per_task[t]
-            for q in 1:model.nLatent
-                x1 = dot(∇Eμ[t][j],μ_f[q])-2*dot(∇EΣ[t][j],μ_f[q].*sum(model.A[t,j,qq]*μ_f[qq] for qq in 1:model.nLatent if qq!=q))
-                x2 = dot(∇EΣ[t][j],abs2.(μ_f[q])+Σ_f[q])
-                new_A[t,j,q] = x1/(2*x2)
-                ∇A[t,j,q] = x1 - 2*model.A[t,j,q]*x2
+    if !isnothing(model.A_opt)
+        μ_f = mean_f.(model.f) # κμ
+        Σ_f = diag_cov_f.(model.f) #Diag(K̃ + κΣκ)
+        ∇Eμ = ∇E_μ.(model.likelihood,model.inference.vi_opt[1:1],get_y(model))
+        ∇EΣ = ∇E_Σ.(model.likelihood,model.inference.vi_opt[1:1],get_y(model))
+        new_A = zero(model.A)
+        ∇A = zero(model.A)
+        for t in 1:model.nTask
+            for j in 1:model.nf_per_task[t]
+                for q in 1:model.nLatent
+                    x1 = dot(∇Eμ[t][j],μ_f[q])-2*dot(∇EΣ[t][j],μ_f[q].*sum(model.A[t,j,qq]*μ_f[qq] for qq in 1:model.nLatent if qq!=q))
+                    x2 = dot(∇EΣ[t][j],abs2.(μ_f[q])+Σ_f[q])
+                    new_A[t,j,q] = x1/(2*x2)
+                    ∇A[t,j,q] = x1 - 2*model.A[t,j,q]*x2
+                end
+                # model.A[t,j,:]./=sum(model.A[t,j,:])
             end
-            # model.A[t,j,:]./=sum(model.A[t,j,:])
         end
+        model.A .+= Flux.Optimise.apply!(model.A_opt,model.A,∇A)
+        # model.A .= model.A./vec(sum(model.A,dims=3))
+        # model.A .= new_A
     end
-    model.A .+= Flux.Optimise.apply!(model.A_opt,model.A,∇A)
-    # model.A .= model.A./vec(sum(model.A,dims=3))
-    # model.A .= new_A
 end
