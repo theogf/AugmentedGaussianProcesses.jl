cec8e414081b0b3935e587834a779b33a6bd26f9
Théo Galy-Fajou <theo.galyfajou@gmail.com>
2020-02-18 10:38:43 +0100

    Moved test of the inverse laplace sampler

---
+++ test/test_invlaplace_sampler.jl
@@ -0,0 +1,15 @@
+using AugmentedGaussianProcesses
+
+ν=3.0
+
+testfunction(x) = (1+x/ν)^(-0.5*(ν+1))
+testfunction(x) = sech(sqrt(x/2))
+testfunction(x) = (1+sqrt(3*x))*exp(-sqrt(3*x))
+testfunction(x) = (1+sqrt(5*x)+5/3*x)*exp(-sqrt(5*x))
+testdist = LaplaceTransformSampler(testfunction,nothing)
+
+laptrans(testdist,n=1)[1]
+p = histogram(rand(testdist,10000),bins=range(0,3,length=100),normalize=true)|>display
+using Distributions
+histogram!(rand(Gamma((ν+1)/2,1/ν),10000),alpha=0.5,bins=range(0,10,length=100),normalize=true)
+histogram!([AugmentedGaussianProcesses.draw(AugmentedGaussianProcesses.PolyaGammaDist(),1,0) for i in 1:10000],alpha=0.5,bins=range(0,10,length=100),normalize=true)
