839fbedb13b70a1e4d845fe7f53fcba233f4a8cd
Théo Galy-Fajou <theo.galyfajou@gmail.com>
2020-03-10 18:57:44 +0100

    Removed obsolete tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,9 +7,11 @@ AGP.setadbackend(:reverse_diff)
 # Global flags for the tests
 @testset "Test for AugmentedGaussianProcesses" begin
 include("test_prior.jl")
-include("test_GP.jl")
-include("test_VGP.jl")
-include("test_SVGP.jl")
-include("test_OnlineSVGP.jl")
+include("test_likelihoods.jl")
+# include("test_analyticVI.jl")
+# include("test_GP.jl")
+# include("test_VGP.jl")
+# include("test_SVGP.jl")
+# include("test_OnlineSVGP.jl")
 # @test include("test_IO.jl")
 end
