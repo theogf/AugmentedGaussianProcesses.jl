c48ed30b11ec6e1df214e6c5460420b57f740d87
Theo Galy-Fajou <theo.galyfajou@gmail.com>
2020-08-02 23:49:35 +0200

    Adding intermediate info messages

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,30 +8,40 @@ include("testingtools.jl")
 AGP.setadbackend(:reverse_diff)
 # Global flags for the tests
 @testset "AugmentedGaussianProcesses.jl tests" begin
-    @info "Passing function tests"
+    @info "Function tests"
     @testset "Functions" begin
         for f in readdir(joinpath(@__DIR__, "functions"))
             include(joinpath("functions",f))
         end
     end
+
+    @info "Hyperparameter tests"
     @testset "Hyperparameter`s" begin
 
     end
+
+    @info "Inference tests"
     @testset "Inference" begin
         for f in readdir(joinpath(@__DIR__, "inference"))
             include(joinpath("inference", f))
         end
     end
+
+    @info "Likelihood tests"
     @testset "Likelihoods" begin
         for f in readdir(joinpath(@__DIR__, "likelihood"))
             include(joinpath("likelihood",f))
         end
     end
+
+    @info "Model tests"
     @testset "Models" begin
         for f in readdir(joinpath(@__DIR__, "models"))
             include(joinpath("models",f))
         end
     end
+
+    @info "Prior tests"
     @testset "Prior" begin
         for f in readdir(joinpath(@__DIR__, "prior"))
             include(joinpath("prior",f))
