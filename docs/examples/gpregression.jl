460267686d735a1275e031429e2f60360cff069a
Théo Galy-Fajou <theo.galyfajou@gmail.com>
2020-11-23 17:11:23 +0100

    Tentative with nbviewer and binder

--- docs/examples/gpregression.jl
+++ docs/examples/gpregression.jl
@@ -1,3 +1,8 @@
+# # Gaussian Process Regression
+# 
+#md # [![](https://mybinder.org/badge_logo.svg)](@__BINDER_ROOT_URL__/examples/gpregression.ipynb)
+#md # [![](https://img.shields.io/badge/show-nbviewer-579ACA.svg)](@__NBVIEWER_ROOT_URL__/examples/gpregression.ipynb)
+
 # ### Loading necessary packages
 using AugmentedGaussianProcesses
 const AGP = AugmentedGaussianProcesses
