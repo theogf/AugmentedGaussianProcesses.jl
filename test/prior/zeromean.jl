c73504ebad1e67be76147d9c0cf9d5b6fe1255bb
Théo Galy-Fajou <theo.galyfajou@gmail.com>
2021-03-30 18:28:27 +0200

    last test correction

--- test/prior/zeromean.jl
+++ test/prior/zeromean.jl
@@ -9,7 +9,7 @@ X = rand(N,D)
 @testset "ZeroMean" begin
         μ₀ = ZeroMean()
         @test ZeroMean() isa ZeroMean{Float64}
-        @test repr(μ₀) == "Zero Mean Prior"
+        @test repr("text/plain", μ₀) == "Zero Mean Prior"
         @test isnothing(AGP.update!(μ₀, []))
         @test all(μ₀(X) .== zeros(N))
         @test μ₀(x) == zero(x)
