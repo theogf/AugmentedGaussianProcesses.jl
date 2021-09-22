0088f75965aa44a2e9d3da89fd3cf63d450ab687
Théo Galy-Fajou <theo.galyfajou@gmail.com>
2021-09-22 12:21:27 +0200

    Fixed multiclass

--- test/likelihood/multiclass.jl
+++ test/likelihood/multiclass.jl
@@ -24,20 +24,17 @@
     AGP.create_mapping!(l, y)
     @test l.class_mapping == ["b", "a", "c"]
     @test l.ind_mapping == Dict("b" => 1, "a" => 2, "c" => 3)
-    y = AGP.create_one_hot(l, y)
-    @test l.Y ==
-          [BitArray([1, 0, 0, 0, 0]), BitArray([0, 1, 0, 1, 1]), BitArray([0, 0, 1, 0, 0])]
+    Y = AGP.create_one_hot(l, y)
+    @test Y == BitMatrix([1 0 0; 0 1 0; 0 0 1; 0 1 0; 0 1 0])
 
     l = LogisticSoftMaxLikelihood(["a", "b", "c"])
     @test l.ind_mapping == Dict("a" => 1, "b" => 2, "c" => 3)
     Y = AGP.create_one_hot(l, y)
-    @test l.Y ==
-          [BitArray([0, 1, 0, 1, 1]), BitArray([1, 0, 0, 0, 0]), BitArray([0, 0, 1, 0, 0])]
+    @test Y == BitMatrix([0 1 0; 1 0 0; 0 0 1; 1 0 0; 1 0 0])
 
     l = LogisticSoftMaxLikelihood(3)
     Y = AGP.treat_labels!(y, l)
     @test l.class_mapping == ["b", "a", "c"]
     @test l.ind_mapping == Dict("b" => 1, "a" => 2, "c" => 3)
-    @test l.Y ==
-          [BitArray([1, 0, 0, 0, 0]), BitArray([0, 1, 0, 1, 1]), BitArray([0, 0, 1, 0, 0])]
+    @test Y == BitMatrix([1 0 0; 0 1 0; 0 0 1; 0 1 0; 0 1 0])
 end
