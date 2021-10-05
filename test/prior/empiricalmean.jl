354b58e3c3ec96f55380cfecf486d5d8912fad36
Théo Galy-Fajou <theo.galyfajou@gmail.com>
2021-10-05 12:17:38 +0200

    Fixed prior tests

--- test/prior/empiricalmean.jl
+++ test/prior/empiricalmean.jl
@@ -2,14 +2,14 @@
     N = 20
     D = 3
     x = rand()
-    X = rand(N, D)
+    X = rand(N)
     v = randn(N)
     μ₀ = EmpiricalMean(v; opt=Descent(1.0))
     st = AGP.init_priormean_state((;), μ₀)
     @test μ₀ isa EmpiricalMean{Float64,Vector{Float64},<:Descent}
     @test repr("text/plain", μ₀) == "Empirical Mean Prior (length(c) = $N)"
     @test μ₀(X) == v
-    @test_throws ErrorException μ₀(rand(N + 1, D))
+    @test_throws ErrorException μ₀(rand(N + 1))
     g = Zygote.gradient(μ₀) do m
         return sum(abs2, m(X))
     end
