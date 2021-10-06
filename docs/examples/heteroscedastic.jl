7f960b35a8152b896d5ea042757b773b5ea11ca9
Théo Galy-Fajou <theo.galyfajou@gmail.com>
2021-10-06 15:37:28 +0200

    Fixes doc fail (#110)
    * Fixes doc fail
    
    * Use Plots.jl
    
    * Fix inconsistency for predictions
    
    * Make gpclassfication faster
    
    * Help with GR issue

--- docs/examples/heteroscedastic.jl
+++ docs/examples/heteroscedastic.jl
@@ -3,6 +3,9 @@
 # ### Loading necessary packages
 using AugmentedGaussianProcesses
 using Distributions
+using LinearAlgebra
+using Plots
+default(lw=3.0, msw=0.0)
 # using CairoMakie
 
 # ## Model generated data
@@ -11,9 +14,10 @@ using Distributions
 # To model this with Gaussian process, we define a GP `f` for the mean and another GP `g` for the variance
 # ``y \sim f + \epsilon``
 # where ``\epsilon \sim \mathcal{N}(0, (\lambda \sigma(g))^{-1})``
-# We create a toy dataset with X ∈ [-20, 20] and sample `f`, `g` and `y` given this same generative model
+# We create a toy dataset with X ∈ [-10, 10] and sample `f`, `g` and `y` given this same generative model
 N = 200
-x = (sort(rand(N)) .- 0.5) * 40.0
+x = (sort(rand(N)) .- 0.5) * 20.0
+x_test = range(-10, 10; length=500)
 kernel = 5.0 * SqExponentialKernel() ∘ ScaleTransform(1.0) # Kernel function
 K = kernelmatrix(kernel, x) + 1e-5I # The kernel matrix
 f = rand(MvNormal(K)) # We draw a random sample from the GP prior
@@ -27,7 +31,7 @@ y = f + σ .* randn(N) # We finally sample the ouput
 # We can visualize the data:
 n_sig = 2 # Number of std. dev. around the mean
 plot(x, f, ribbon = n_sig * σ, lab= "p(y|f,g)") # Mean and std. dev. of y
-scatter!(x, y, alpha=0.2, msw=0.0, lab="y") # Observation samples
+scatter!(x, y, alpha=0.5, msw=0.0, lab="y") # Observation samples
 
 # ## Model creation and training
 # We will now use the augmented model to infer both `f` and `g`
@@ -47,16 +51,16 @@ train!(model, 100);
 
 # ## Predictions 
 # We can now look at the predictions and compare them with out original model
-(f_m, g_m), (f_σ, g_σ) = predict_f(model, x; cov=true)
-y_m, y_σ = proba_y(model, x)
+(f_m, g_m), (f_σ, g_σ) = predict_f(model, x_test; cov=true)
+y_m, y_σ = proba_y(model, x_test)
 # Let's first look at the differece between the latent `f` and `g`
 plot(x, [f, g]; label=["f" "g"])
-plot!(x, [f_m, g_m]; ribbon=[n_sig * f_σ, n_sig * g_σ], label=["f_pred" "g_pred"])
+plot!(x_test, [f_m, g_m]; ribbon=[n_sig * f_σ n_sig * g_σ], lab=["f_pred" "g_pred"], legend=true)
 # But it's more interesting to compare the predictive probability of `y` directly:
 plot(x, f; ribbon = n_sig * σ, lab="p(y|f,g)")
-plot!(x, y_m, ribbon = n_sig * sqrt.(y_σ), lab="p(y|f,g) pred")
-scatter!(x, y; lab="y", msw=0.0, alpha=0.2)
+plot!(x_test, y_m, ribbon = n_sig * sqrt.(y_σ), lab="p(y|f,g) pred")
+scatter!(x, y; lab="y", alpha=0.2)
 # Or to explore the heteroscedasticity itself, we can look at the residuals
-scatter(x, (f - y).^2; yaxis=:log, lab="residuals", msw=0.0, alpha=0.2)
-plot!(x, σ .^ 2; lab="true σ²(x)", lw=3.0)
-plot!(x, y_σ; lab="predicted σ²(x)", lw=3.0)
+scatter(x, (f - y).^2; yaxis=:log, lab="residuals",alpha=0.2)
+plot!(x, σ .^ 2; lab="true σ²(x)")
+plot!(x_test, y_σ; lab="predicted σ²(x)")
